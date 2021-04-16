106bef3c1ce588cb912fa03590bda768984a4814
Beth <bethhnielsen56@gmail.com>
2021-04-16 13:36:04 -0500

    answers are displaying and shuffling

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,7 +5,6 @@ import QuestionComponent from '../QuestionComponent/QuestionComponent';
 class Home extends React.Component {
   state = {
     questions: [],
-    answers: [],
   }
 
   getTriviaQuestions = () => {
@@ -14,22 +13,8 @@ class Home extends React.Component {
       .catch((err) => console.error('unable to get trivia questions'));
   }
 
-  // get questions
-  // select both correct_answer and incorrect_answers
-  // push incorrect_answers into a new array called answerOptions
-  // push correct_answer into answerOptions
-  // somehow randomize these
-
-  getTriviaAnswers = () => {
-    const { questions, answers } = this.state;
-    questions.map((q) => q={answers});
-    answers.push(q.correct_answer, q.incorrect_answers);
-    console.error(answers);
-  }
-
   componentDidMount() {
     this.getTriviaQuestions();
-    // this.getTriviaAnswers();
   }
 
   render() {
--- src/Components/QuestionComponent/QuestionComponent.js
+++ src/Components/QuestionComponent/QuestionComponent.js
@@ -1,20 +1,52 @@
 import React from 'react';
-import trivia from '../../helpers/data/triviaData';
+
+const shuffle = require('shuffle-array');
 
 class QuestionComponent extends React.Component {
   state = {
     answers: [],
   }
 
-  render() {
-    const { question, answers } = this.props;
+  //   category: "Entertainment: Music"
+  // correct_answer: "Humanz"
+  // difficulty: "medium"
+  // incorrect_answers: (3) ["Plastic Beach", "The Fall", "Demon Days"]
+  // question: "What album did Gorillaz release in 2017?"
+  // type: "multiple"
 
-    console.error('answers', answers);
+  // get questions
+  // select both correct_answer and incorrect_answers
+  // push incorrect_answers into a new array called answerOptions
+  // push correct_answer into answerOptions
+  // somehow randomize these
+
+  getTriviaAnswers = () => {
+    const { answers } = this.state;
+    const { question } = this.props;
+    question.incorrect_answers.forEach((answer) => {
+      answers.push(answer);
+    });
+    answers.push(question.correct_answer);
+    shuffle(answers);
+    console.error('answers shuffled?', answers);
+    this.setState({ answers });
+  }
+
+  componentDidMount() {
+    this.getTriviaAnswers();
+  }
+
+  render() {
+    const { question } = this.props;
+    const { answers } = this.state;
+
     console.error('question from q component', question);
     return (
       <div>
         <h1> Question Component</h1>
         <h1>{question.category}</h1>
+        <p>{question.question}</p>
+        <p>{answers}</p>
       </div>
     );
   }
